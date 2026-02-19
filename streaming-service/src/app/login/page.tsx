706387f55ac4ea9ebbc2e3190ba511f2e0b5884c
Kostiantyn Sotnikov <sotnik2804@gmail.com>
2026-02-19 14:06:25 +0100

    change local ip to localhost

--- streaming-service/src/app/login/page.tsx
+++ streaming-service/src/app/login/page.tsx
@@ -11,7 +11,7 @@ export default function AccountPage() {
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const res = await fetch('http://127.0.0.1:5000/login', {
+      const res = await fetch('http://localhost:5000/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
